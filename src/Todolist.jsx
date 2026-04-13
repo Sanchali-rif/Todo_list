e0ba5315f5e5d9021a2445d60cdcca75b7b0a1f6
Sanchali-rif <sanchalisaha05@gmail.com>
2026-04-13 23:14:02 +0530

    function setup

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -1,6 +1,27 @@
 import React, { usestate } from "react";
 function Todolist() {
 
-    return (<></>);
+    const [Tasks, setTasks] = usestate([]);
+    const [newTask, setNewTask] = usestate("");
+    function handleInputChange(event) {
+
+    }
+    function addTask() {
+
+    }
+    function deleteTask(index) {
+
+    }
+    function moveTaskUp(index) {
+
+    }
+    function moveTaskDown(index) {
+
+    }
+    return (
+        <div className="to-do-list">
+            <h1>Todo-List</h1>
+        </div>
+    );
 }
 export default Todolist
